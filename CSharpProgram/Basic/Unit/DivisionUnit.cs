8fb5d7f68783f47bc08cd68e90460dda7897948e
hshan <hhs900110@gmail.com>
2016-04-11 22:04:00 +0900

    Project Euler A 0017 Miss File

---
+++ CSharpProgram/Basic/Unit/DivisionUnit.cs
@@ -0,0 +1,26 @@
+﻿namespace Unit
+{
+    public struct DivisionUnit
+    {
+        private ulong m_remainder;
+        private ulong m_quotien;
+
+        public DivisionUnit(ulong target, ulong division)
+        {
+            m_remainder = 0;
+            m_quotien = 0;
+            SetDivisionUnit(target, division);
+        }
+
+        public void SetDivisionUnit(ulong target, ulong division)
+        {
+            m_remainder = target % division;
+            m_quotien = (target - m_remainder) / division;
+        }
+
+        /// <summary> 나눈 값의 나머지 </summary>
+        public ulong Remainder { get { return m_remainder; } }
+        /// <summary> 나눈 값의 몫 </summary>
+        public ulong Quotien { get { return m_quotien; } }
+    }
+}
